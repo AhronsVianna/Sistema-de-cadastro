782f7daeab75d251e3cc5093442d5464a515376e
AhronsVianna <ahronsvianna@gmail.com>
2023-06-07 13:41:50 -0300

    commit suspostamente feito por outra pessoa

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log teste
+console.log("O Lucas, fingindo ser outra pessoa, fez esse commit sem você ver na aula")
